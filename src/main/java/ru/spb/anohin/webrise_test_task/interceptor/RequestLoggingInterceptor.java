d117d4bcb228fc787beab928bb2b5d66a13ad059
Anokhin <a.i.anohin.arduino@gmail.com>
2025-05-10 16:24:22 +0300

    Логи в БД через Interseptor

---
+++ src/main/java/ru/spb/anohin/webrise_test_task/interceptor/RequestLoggingInterceptor.java
@@ -0,0 +1,20 @@
+package ru.spb.anohin.webrise_test_task.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class RequestLoggingInterceptor implements HandlerInterceptor {
+
+    private static final Logger log = LoggerFactory.getLogger(RequestLoggingInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("{} {} вызван", request.getMethod(), request.getRequestURI());
+        return true;
+    }
+}
